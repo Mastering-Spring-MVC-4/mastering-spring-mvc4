bde1b85e425d7e200f9b6b754f17d05912cd2f13
geowarin <geowarin@gmail.com>
2015-07-06 13:35:54 +0200

    Generated project with IntelliJ and generated gradle wrapper

---
+++ src/main/java/masterSpringMvc/MasterSpringMvcApplication.java
@@ -0,0 +1,12 @@
+package masterSpringMvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterSpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterSpringMvcApplication.class, args);
+    }
+}
