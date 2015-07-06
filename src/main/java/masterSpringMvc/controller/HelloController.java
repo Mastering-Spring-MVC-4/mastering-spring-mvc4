9484c01e75d09c3ec98da094ec2f81e4355cb601
geowarin <geowarin@gmail.com>
2015-07-06 13:40:51 +0200

    Added the hello controller

---
+++ src/main/java/masterSpringMvc/controller/HelloController.java
@@ -0,0 +1,15 @@
+package masterSpringMvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String hello() {
+        return "Hello, world!";
+    }
+}
