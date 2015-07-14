51fd929a9fe5fc1a642d322d1987a9139b3c13e9
geowarin <geowarin@gmail.com>
2015-07-14 22:25:49 +0200

    Binding the DTO to the form

--- src/main/java/masterSpringMvc/profile/ProfileController.java
+++ src/main/java/masterSpringMvc/profile/ProfileController.java
@@ -2,12 +2,19 @@ package masterSpringMvc.profile;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ProfileController {
 
     @RequestMapping("/profile")
-    public String displayProfile() {
+    public String displayProfile(ProfileForm profileForm) {
         return "profile/profilePage";
+    }
+
+    @RequestMapping(value = "/profile", method = RequestMethod.POST)
+    public String saveProfile(ProfileForm profileForm) {
+        System.out.println("save ok: " + profileForm);
+        return "redirect:/profile";
     }
 }
---
+++ src/main/java/masterSpringMvc/profile/ProfileForm.java
@@ -0,0 +1,53 @@
+package masterSpringMvc.profile;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProfileForm {
+    private String twitterHandle;
+    private String email;
+    private LocalDate birthDate;
+    private List<String> tastes = new ArrayList<>();
+
+    public String getTwitterHandle() {
+        return twitterHandle;
+    }
+
+    public void setTwitterHandle(String twitterHandle) {
+        this.twitterHandle = twitterHandle;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public List<String> getTastes() {
+        return tastes;
+    }
+
+    public void setTastes(List<String> tastes) {
+        this.tastes = tastes;
+    }
+
+    @Override public String toString() {
+        return "ProfileForm{" +
+                "twitterHandle='" + twitterHandle + '\'' +
+                ", email='" + email + '\'' +
+                ", birthDate=" + birthDate +
+                ", tastes=" + tastes +
+                '}';
+    }
+}
