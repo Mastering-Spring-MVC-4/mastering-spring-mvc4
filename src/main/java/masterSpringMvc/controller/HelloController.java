56a665545ff34a361c18cd84b013e8a948817cfc
geowarin <geowarin@gmail.com>
2015-07-14 19:26:47 +0200

    First page with thymeleaf

--- src/main/java/masterSpringMvc/controller/HelloController.java
+++ src/main/java/masterSpringMvc/controller/HelloController.java
@@ -2,14 +2,12 @@ package masterSpringMvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
     @RequestMapping("/")
-    @ResponseBody
     public String hello() {
-        return "Hello, world!";
+        return "resultPage";
     }
 }
