5c463c1ba4ba4072dac93b5cfa0ef3da574e658c
geowarin <geowarin@gmail.com>
2015-07-14 19:35:10 +0200

    Using a request parameter

--- src/main/java/masterSpringMvc/controller/HelloController.java
+++ src/main/java/masterSpringMvc/controller/HelloController.java
@@ -3,13 +3,14 @@ package masterSpringMvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
     @RequestMapping("/")
-    public String hello(Model model) {
-        model.addAttribute("message", "Hello from the controller");
+    public String hello(@RequestParam(defaultValue = "world") String name, Model model) {
+        model.addAttribute("message", "Hello, " + name);
         return "resultPage";
     }
 }
