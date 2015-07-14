76b1d2c469c9bbd0a210d5a351054a0af2a21be7
geowarin <geowarin@gmail.com>
2015-07-14 19:32:29 +0200

    Passing data to the view

--- src/main/java/masterSpringMvc/controller/HelloController.java
+++ src/main/java/masterSpringMvc/controller/HelloController.java
@@ -1,13 +1,15 @@
 package masterSpringMvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
 
     @RequestMapping("/")
-    public String hello() {
+    public String hello(Model model) {
+        model.addAttribute("message", "Hello from the controller");
         return "resultPage";
     }
 }
