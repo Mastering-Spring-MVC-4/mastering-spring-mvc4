5b415db537b2c3db8e0d959f280532199efec75a
geowarin <geowarin@gmail.com>
2015-07-14 20:04:28 +0200

    Simple search

--- src/main/java/masterSpringMvc/controller/TweetController.java
+++ src/main/java/masterSpringMvc/controller/TweetController.java
@@ -18,6 +18,11 @@ public class TweetController {
     private Twitter twitter;
 
     @RequestMapping("/")
+    public String home() {
+        return "searchPage";
+    }
+
+    @RequestMapping("/result")
     public String hello(@RequestParam(defaultValue = "masterSpringMVC4") String search, Model model) {
         SearchResults searchResults = twitter.searchOperations().search(search);
         List<Tweet> tweets = searchResults.getTweets();
