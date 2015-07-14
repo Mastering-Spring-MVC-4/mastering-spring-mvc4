c4f42f40f9d6fce13fadb078228428c0e724287a
geowarin <geowarin@gmail.com>
2015-07-14 19:52:14 +0200

    Display a list of tweets

--- src/main/java/masterSpringMvc/controller/TweetController.java
+++ src/main/java/masterSpringMvc/controller/TweetController.java
@@ -2,11 +2,15 @@ package masterSpringMvc.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.twitter.api.SearchResults;
+import org.springframework.social.twitter.api.Tweet;
 import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class TweetController {
@@ -17,8 +21,12 @@ public class TweetController {
     @RequestMapping("/")
     public String hello(@RequestParam(defaultValue = "masterSpringMVC4") String search, Model model) {
         SearchResults searchResults = twitter.searchOperations().search(search);
-        String text = searchResults.getTweets().get(0).getText();
-        model.addAttribute("message", text);
+        List<String> tweets =
+                searchResults.getTweets()
+                        .stream()
+                        .map(Tweet::getText)
+                        .collect(Collectors.toList());
+        model.addAttribute("tweets", tweets);
         return "resultPage";
     }
 }
