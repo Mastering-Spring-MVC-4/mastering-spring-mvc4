a25c418da0aa66bf536236ced0757bdc9410255f
geowarin <geowarin@gmail.com>
2015-07-26 22:46:40 +0200

    Basic validation

--- src/main/java/masterSpringMvc/profile/ProfileController.java
+++ src/main/java/masterSpringMvc/profile/ProfileController.java
@@ -2,10 +2,12 @@ package masterSpringMvc.profile;
 
 import masterSpringMvc.date.USLocalDateFormatter;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.validation.Valid;
 import java.util.Locale;
 
 @Controller
@@ -22,8 +24,12 @@ public class ProfileController {
     }
 
     @RequestMapping(value = "/profile", method = RequestMethod.POST)
-    public String saveProfile(ProfileForm profileForm) {
-        System.out.println("save ok: " + profileForm);
+    public String saveProfile(@Valid ProfileForm profileForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "profile/profilePage";
+        }
+
+        System.out.println("save ok" + profileForm);
         return "redirect:/profile";
     }
 }
--- src/main/java/masterSpringMvc/profile/ProfileForm.java
+++ src/main/java/masterSpringMvc/profile/ProfileForm.java
@@ -1,13 +1,23 @@
 package masterSpringMvc.profile;
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ProfileForm {
+    @Size(min = 2)
     private String twitterHandle;
+    @Email
+    @NotEmpty
     private String email;
+    @NotNull
     private LocalDate birthDate;
+    @NotEmpty
     private List<String> tastes = new ArrayList<>();
 
     public String getTwitterHandle() {
