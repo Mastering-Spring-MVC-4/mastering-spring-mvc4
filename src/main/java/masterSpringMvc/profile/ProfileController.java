4dbe0b567856c9dacd9e529f6068999bc532418d
geowarin <geowarin@gmail.com>
2015-07-26 22:40:28 +0200

    Add date format place holder

--- src/main/java/masterSpringMvc/profile/ProfileController.java
+++ src/main/java/masterSpringMvc/profile/ProfileController.java
@@ -1,11 +1,20 @@
 package masterSpringMvc.profile;
 
+import masterSpringMvc.date.USLocalDateFormatter;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.Locale;
+
 @Controller
 public class ProfileController {
+
+    @ModelAttribute("dateFormat")
+    public String localeFormat(Locale locale) {
+        return USLocalDateFormatter.getPattern(locale);
+    }
 
     @RequestMapping("/profile")
     public String displayProfile(ProfileForm profileForm) {
