c97877e234070277bece44516efe57a16773e465
geowarin <geowarin@gmail.com>
2015-07-28 13:54:54 +0200

    Check if uploaded file is an image

--- src/main/java/masterSpringMvc/profile/PictureUploadController.java
+++ src/main/java/masterSpringMvc/profile/PictureUploadController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.*;
 
@@ -20,16 +21,31 @@ public class PictureUploadController {
     }
 
     @RequestMapping(value = "/upload", method = RequestMethod.POST)
-    public String onUpload(MultipartFile file) throws IOException {
-        String filename = file.getOriginalFilename();
-        File tempFile = File.createTempFile("pic", getFileExtension(filename), PICTURES_DIR.getFile());
+    public String onUpload(MultipartFile file, RedirectAttributes redirectAttrs) throws IOException {
 
-        try (InputStream in = file.getInputStream();
-             OutputStream out = new FileOutputStream(tempFile)) {
-            IOUtils.copy(in, out);
+        if (file.isEmpty() || !isImage(file)) {
+            redirectAttrs.addFlashAttribute("error", "Incorrect file. Please upload a picture.");
+            return "redirect:/upload";
         }
 
+        copyFileToPictures(file);
+
         return "profile/uploadPage";
+    }
+
+    private Resource copyFileToPictures(MultipartFile file) throws IOException {
+        String fileExtension = getFileExtension(file.getOriginalFilename());
+        File tempFile = File.createTempFile("pic", fileExtension, PICTURES_DIR.getFile());
+        try (InputStream in = file.getInputStream();
+             OutputStream out = new FileOutputStream(tempFile)) {
+
+            IOUtils.copy(in, out);
+        }
+        return new FileSystemResource(tempFile);
+    }
+
+    private boolean isImage(MultipartFile file) {
+        return file.getContentType().startsWith("image");
     }
 
     private static String getFileExtension(String name) {
