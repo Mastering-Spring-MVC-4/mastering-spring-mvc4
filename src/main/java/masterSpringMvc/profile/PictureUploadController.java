02f3add37bec49c1b0a5a6662ddceab145d90577
geowarin <geowarin@gmail.com>
2015-07-29 13:56:56 +0200

    Handled IOException

--- src/main/java/masterSpringMvc/profile/PictureUploadController.java
+++ src/main/java/masterSpringMvc/profile/PictureUploadController.java
@@ -7,11 +7,9 @@ import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletResponse;
@@ -50,6 +48,8 @@ public class PictureUploadController {
     @RequestMapping(value = "/upload", method = RequestMethod.POST)
     public String onUpload(MultipartFile file, RedirectAttributes redirectAttrs, Model model) throws IOException {
 
+        // uncomment to see the IOException is handled
+//        throw new IOException("Some message");
         if (file.isEmpty() || !isImage(file)) {
             redirectAttrs.addFlashAttribute("error", "Incorrect file. Please upload a picture.");
             return "redirect:/upload";
@@ -70,6 +70,13 @@ public class PictureUploadController {
             IOUtils.copy(in, out);
         }
         return new FileSystemResource(tempFile);
+    }
+
+    @ExceptionHandler(IOException.class)
+    public ModelAndView handleIOException(IOException exception) {
+        ModelAndView modelAndView = new ModelAndView("profile/uploadPage");
+        modelAndView.addObject("error", exception.getMessage());
+        return modelAndView;
     }
 
     private boolean isImage(MultipartFile file) {
