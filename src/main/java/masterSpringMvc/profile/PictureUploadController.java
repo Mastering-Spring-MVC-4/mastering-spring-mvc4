efc77a6d11e301f6d9b4fd2a17751a1272a7eda2
geowarin <geowarin@gmail.com>
2015-08-01 19:23:21 +0200

    Put the picture upload on the profile form and the image path in session

--- src/main/java/masterSpringMvc/profile/PictureUploadController.java
+++ src/main/java/masterSpringMvc/profile/PictureUploadController.java
@@ -7,8 +7,10 @@ import org.springframework.context.MessageSource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -19,50 +21,44 @@ import java.net.URLConnection;
 import java.util.Locale;
 
 @Controller
-@SessionAttributes("picturePath")
 public class PictureUploadController {
     private final Resource picturesDir;
     private final Resource anonymousPicture;
     private final MessageSource messageSource;
+    private final UserProfileSession userProfileSession;
 
     @Autowired
-    public PictureUploadController(PictureUploadProperties uploadProperties, MessageSource messageSource) {
+    public PictureUploadController(PictureUploadProperties uploadProperties,
+                                   MessageSource messageSource,
+                                   UserProfileSession userProfileSession) {
         picturesDir = uploadProperties.getUploadPath();
         anonymousPicture = uploadProperties.getAnonymousPicture();
         this.messageSource = messageSource;
-    }
-
-    @ModelAttribute("picturePath")
-    public Resource picturePath() {
-        return anonymousPicture;
-    }
-
-    @RequestMapping("upload")
-    public String uploadPage() {
-        return "profile/uploadPage";
+        this.userProfileSession = userProfileSession;
     }
 
     @RequestMapping(value = "/uploadedPicture")
-    public void getUploadedPicture(HttpServletResponse response,
-                                   @ModelAttribute("picturePath") Resource picturePath) throws IOException {
+    public void getUploadedPicture(HttpServletResponse response) throws IOException {
+        Resource picturePath = userProfileSession.getPicturePath();
+        if (picturePath == null) {
+            picturePath = anonymousPicture;
+        }
         response.setHeader("Content-Type", URLConnection.guessContentTypeFromName(picturePath.getFilename()));
         IOUtils.copy(picturePath.getInputStream(), response.getOutputStream());
     }
 
-    @RequestMapping(value = "/upload", method = RequestMethod.POST)
-    public String onUpload(MultipartFile file, RedirectAttributes redirectAttrs, Model model) throws IOException {
+    @RequestMapping(value = "/profile", params = {"upload"}, method = RequestMethod.POST)
+    public String onUpload(@RequestParam MultipartFile file, RedirectAttributes redirectAttrs) throws IOException {
 
-        // uncomment to see the IOException is handled
-//        throw new IOException("Some message");
         if (file.isEmpty() || !isImage(file)) {
             redirectAttrs.addFlashAttribute("error", "Incorrect file. Please upload a picture.");
-            return "redirect:/upload";
+            return "redirect:/profile";
         }
 
         Resource picturePath = copyFileToPictures(file);
-        model.addAttribute("picturePath", picturePath);
+        userProfileSession.setPicturePath(picturePath);
 
-        return "profile/uploadPage";
+        return "redirect:profile";
     }
 
     private Resource copyFileToPictures(MultipartFile file) throws IOException {
@@ -78,15 +74,17 @@ public class PictureUploadController {
 
     @ExceptionHandler(IOException.class)
     public ModelAndView handleIOException(Locale locale) {
-        ModelAndView modelAndView = new ModelAndView("profile/uploadPage");
+        ModelAndView modelAndView = new ModelAndView("profile/profilePage");
         modelAndView.addObject("error", messageSource.getMessage("upload.io.exception", null, locale));
+        modelAndView.addObject("profileForm", userProfileSession.toForm());
         return modelAndView;
     }
 
     @RequestMapping("uploadError")
     public ModelAndView onUploadError(Locale locale) {
-        ModelAndView modelAndView = new ModelAndView("profile/uploadPage");
+        ModelAndView modelAndView = new ModelAndView("profile/profilePage");
         modelAndView.addObject("error", messageSource.getMessage("upload.file.too.big", null, locale));
+        modelAndView.addObject("profileForm", userProfileSession.toForm());
         return modelAndView;
     }
 
--- src/main/java/masterSpringMvc/profile/UserProfileSession.java
+++ src/main/java/masterSpringMvc/profile/UserProfileSession.java
@@ -2,9 +2,13 @@ package masterSpringMvc.profile;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.io.Serializable;
+import java.net.URL;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +20,7 @@ public class UserProfileSession implements Serializable {
     private String email;
     private LocalDate birthDate;
     private List<String> tastes = new ArrayList<>();
+    private URL picturePath;
 
     public void saveForm(ProfileForm profileForm) {
         this.twitterHandle = profileForm.getTwitterHandle();
@@ -31,5 +36,13 @@ public class UserProfileSession implements Serializable {
         profileForm.setBirthDate(birthDate);
         profileForm.setTastes(tastes);
         return profileForm;
+    }
+
+    public void setPicturePath(Resource picturePath) throws IOException {
+        this.picturePath = picturePath.getURL();
+    }
+
+    public Resource getPicturePath() {
+        return picturePath == null ? null : new UrlResource(picturePath);
     }
 }
