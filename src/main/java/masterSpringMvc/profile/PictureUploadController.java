915840936107845d9ef56ec01dc2076475574e8a
geowarin <geowarin@gmail.com>
2015-08-01 17:26:52 +0200

    Display localized messages when an error occurs when uploading a file

--- src/main/java/masterSpringMvc/profile/PictureUploadController.java
+++ src/main/java/masterSpringMvc/profile/PictureUploadController.java
@@ -3,6 +3,7 @@ package masterSpringMvc.profile;
 import masterSpringMvc.config.PictureUploadProperties;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
@@ -11,23 +12,24 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.util.WebUtils;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.net.URLConnection;
+import java.util.Locale;
 
 @Controller
 @SessionAttributes("picturePath")
 public class PictureUploadController {
     private final Resource picturesDir;
     private final Resource anonymousPicture;
+    private final MessageSource messageSource;
 
     @Autowired
-    public PictureUploadController(PictureUploadProperties uploadProperties) {
+    public PictureUploadController(PictureUploadProperties uploadProperties, MessageSource messageSource) {
         picturesDir = uploadProperties.getUploadPath();
         anonymousPicture = uploadProperties.getAnonymousPicture();
+        this.messageSource = messageSource;
     }
 
     @ModelAttribute("picturePath")
@@ -75,16 +77,16 @@ public class PictureUploadController {
     }
 
     @ExceptionHandler(IOException.class)
-    public ModelAndView handleIOException(IOException exception) {
+    public ModelAndView handleIOException(Locale locale) {
         ModelAndView modelAndView = new ModelAndView("profile/uploadPage");
-        modelAndView.addObject("error", exception.getMessage());
+        modelAndView.addObject("error", messageSource.getMessage("upload.io.exception", null, locale));
         return modelAndView;
     }
 
     @RequestMapping("uploadError")
-    public ModelAndView onUploadError(HttpServletRequest request) {
+    public ModelAndView onUploadError(Locale locale) {
         ModelAndView modelAndView = new ModelAndView("profile/uploadPage");
-        modelAndView.addObject("error", request.getAttribute(WebUtils.ERROR_MESSAGE_ATTRIBUTE));
+        modelAndView.addObject("error", messageSource.getMessage("upload.file.too.big", null, locale));
         return modelAndView;
     }
 
