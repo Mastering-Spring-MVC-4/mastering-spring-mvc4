30b71892047b9a5f9190de522db8b0c17a1cea95
geowarin <geowarin@gmail.com>
2015-08-02 22:02:11 +0200

    Add HTTP basic auth

---
+++ src/main/java/masterSpringMvc/config/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package masterSpringMvc.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureAuth(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("user").roles("USER").and()
+                .withUser("admin").password("admin").roles("USER", "ADMIN");
+    }
+}
