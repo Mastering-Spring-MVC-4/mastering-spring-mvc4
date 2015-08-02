c8203659045e24e3d8104f6b22208ee7b0b52e19
geowarin <geowarin@gmail.com>
2015-08-02 22:04:03 +0200

    Configure security for the API
    Users can do read only operation (GET)
    Admin can do write operations (POST, PUT, DELETE)

--- src/main/java/masterSpringMvc/config/SecurityConfiguration.java
+++ src/main/java/masterSpringMvc/config/SecurityConfiguration.java
@@ -2,8 +2,10 @@ package masterSpringMvc.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
@@ -16,5 +18,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
                 .withUser("user").password("user").roles("USER").and()
                 .withUser("admin").password("admin").roles("USER", "ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic()
+                .and()
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/login", "/logout").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/**").hasRole("USER")
+                .antMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/api/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN")
+                .anyRequest().authenticated();
     }
 }
