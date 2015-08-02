15fb3d518fbac5cb88580f3d5bf4438d1d53fc07
geowarin <geowarin@gmail.com>
2015-08-02 12:32:55 +0200

    Add a simple REST controller for twitter search

---
+++ src/main/java/masterSpringMvc/search/api/SearchApiController.java
@@ -0,0 +1,24 @@
+package masterSpringMvc.search.api;
+
+import masterSpringMvc.search.SearchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.twitter.api.Tweet;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/search")
+public class SearchApiController {
+    private SearchService searchService;
+
+    @Autowired
+    public SearchApiController(SearchService searchService) {
+        this.searchService = searchService;
+    }
+
+    @RequestMapping(value = "/{searchType}", method = RequestMethod.GET)
+    public List<Tweet> search(@PathVariable String searchType, @MatrixVariable List<String> keywords) {
+        return searchService.search(searchType, keywords);
+    }
+}
