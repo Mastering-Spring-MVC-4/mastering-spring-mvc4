1c7d7330815637e19bb9c0f8318863ae5fbf8d59
geowarin <geowarin@gmail.com>
2015-08-02 22:20:42 +0200

    Add a custom login page

---
+++ src/main/java/masterSpringMvc/authentication/LoginController.java
@@ -0,0 +1,13 @@
+package masterSpringMvc.authentication;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/login")
+    public String authenticate() {
+        return "login";
+    }
+}
--- src/main/java/masterSpringMvc/config/WebSecurityConfiguration.java
+++ src/main/java/masterSpringMvc/config/WebSecurityConfiguration.java
@@ -11,6 +11,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .formLogin()
+                .loginPage("/login")
                 .defaultSuccessUrl("/profile")
                 .and()
                 .logout().logoutSuccessUrl("/login")
