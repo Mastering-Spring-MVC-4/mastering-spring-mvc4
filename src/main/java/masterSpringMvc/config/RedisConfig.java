08bd3fca3e98fff24ef1d247daaa734adb0f20a2
geowarin <geowarin@gmail.com>
2015-08-02 23:07:52 +0200

    Add distributed sessions with Redis
    Using spring session, we are able to easily serialize the sessions inside of redis which
    means they can be shared across multiple servers.
    
    Launch the application with -Dspring.profiles.active=redis and it will try to connect to
    a local redis server

---
+++ src/main/java/masterSpringMvc/config/RedisConfig.java
@@ -0,0 +1,11 @@
+package masterSpringMvc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@Profile("redis")
+@EnableRedisHttpSession
+public class RedisConfig {
+}
