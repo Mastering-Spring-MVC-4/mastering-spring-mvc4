a67b7305abc0d2faa63cceab38f72fbe9592a907
geowarin <geowarin@gmail.com>
2015-08-02 19:39:50 +0200

    Add api documentation with spring-fox (swagger)
    The documentation is available at http://localhost:8080/swagger-ui.html

--- src/main/java/masterSpringMvc/config/WebConfiguration.java
+++ src/main/java/masterSpringMvc/config/WebConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomi
 import org.springframework.boot.context.embedded.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.multipart.MultipartException;
@@ -17,10 +18,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.util.UrlPathHelper;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.time.LocalDate;
 
 @Configuration
+@EnableSwagger2
 public class WebConfiguration extends WebMvcConfigurerAdapter {
 
     @Override
@@ -59,6 +64,16 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
     }
 
     @Bean
+    public Docket userApi() {
+        // the api documentation is available at http://localhost:8080/swagger-ui.html
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(path -> path.startsWith("/api/"))
+                .build();
+    }
+
+    @Bean
+    @Primary
     public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
         ObjectMapper objectMapper = builder.createXmlMapper(false).build();
         objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
