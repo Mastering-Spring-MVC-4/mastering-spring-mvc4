5acb68b82825fdc7480d89ef0b9fcd523e5c9c26
geowarin <geowarin@gmail.com>
2015-08-03 04:47:30 +0200

    SearchApiControllerTest with JSON assert

---
+++ src/test/java/masterSpringMvc/search/api/SearchApiControllerTest.java
@@ -0,0 +1,52 @@
+package masterSpringMvc.search.api;
+
+import masterSpringMvc.MasterSpringMvcApplication;
+import masterSpringMvc.search.StubTwitterSearchConfig;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = {
+        MasterSpringMvcApplication.class,
+        StubTwitterSearchConfig.class
+})
+@WebAppConfiguration
+public class SearchApiControllerTest {
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void should_search() throws Exception {
+
+        this.mockMvc.perform(
+                get("/api/search/mixed;keywords=spring")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].text", is("tweetText")))
+                .andExpect(jsonPath("$[1].text", is("secondTweet")));
+    }
+}
