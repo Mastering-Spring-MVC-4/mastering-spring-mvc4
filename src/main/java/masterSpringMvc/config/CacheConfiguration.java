36d41e9cd085437c6c875dc5206d0168ea8fac31
geowarin <geowarin@gmail.com>
2015-08-03 18:23:13 +0200

    Add cache invalidation with guava cache

--- src/main/java/masterSpringMvc/config/CacheConfiguration.java
+++ src/main/java/masterSpringMvc/config/CacheConfiguration.java
@@ -1,13 +1,13 @@
 package masterSpringMvc.config;
 
+import com.google.common.cache.CacheBuilder;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCache;
-import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.cache.guava.GuavaCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableCaching
@@ -15,10 +15,13 @@ public class CacheConfiguration {
 
     @Bean
     public CacheManager cacheManager() {
-        SimpleCacheManager simpleCacheManager = new SimpleCacheManager();
-        simpleCacheManager.setCaches(Arrays.asList(
-                new ConcurrentMapCache("searches")
-        ));
-        return simpleCacheManager;
+        GuavaCacheManager cacheManager = new GuavaCacheManager("searches");
+        cacheManager
+                .setCacheBuilder(
+                        CacheBuilder.newBuilder()
+                                .softValues()
+                                .expireAfterWrite(10, TimeUnit.MINUTES)
+                );
+        return cacheManager;
     }
 }
