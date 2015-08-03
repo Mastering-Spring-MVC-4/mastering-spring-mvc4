9bb49fdccbee633846b9f56ebd3b591abebe5c6d
geowarin <geowarin@gmail.com>
2015-08-03 10:54:16 +0200

    Add a login and fill form integration test

--- src/integrationTest/java/FluentIntegrationTest.java
+++ src/integrationTest/java/FluentIntegrationTest.java
@@ -1,4 +1,5 @@
 import masterSpringMvc.MasterSpringMvcApplication;
+import masterSpringMvc.auth.StubSocialSigninConfig;
 import masterSpringMvc.search.StubTwitterSearchConfig;
 import org.fluentlenium.adapter.FluentTest;
 import org.junit.Test;
@@ -11,11 +12,13 @@ import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.fluentlenium.core.filter.FilterConstructor.withName;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = {
         MasterSpringMvcApplication.class,
-        StubTwitterSearchConfig.class
+        StubTwitterSearchConfig.class,
+        StubSocialSigninConfig.class
 })
 @WebIntegrationTest(randomPort = true)
 public class FluentIntegrationTest extends FluentTest {
@@ -36,5 +39,27 @@ public class FluentIntegrationTest extends FluentTest {
     public void hasPageTitle() {
         goTo("/");
         assertThat(findFirst("h2").getText()).isEqualTo("Login");
+    }
+
+    @Test
+    public void should_be_redirected_after_filling_form() {
+        goTo("/");
+        assertThat(findFirst("h2").getText()).isEqualTo("Login");
+
+        find("button", withName("twitterSignin")).click();
+        assertThat(findFirst("h2").getText()).isEqualTo("Your profile");
+
+        fill("#twitterHandle").with("geowarin");
+        fill("#email").with("geowarin@mymail.com");
+        fill("#birthDate").with("03/19/1987");
+
+        find("button", withName("addTaste")).click();
+        fill("#tastes0").with("spring");
+
+        find("button", withName("save")).click();
+
+        takeScreenShot();
+        assertThat(findFirst("h2").getText()).isEqualTo("Tweet results for spring");
+        assertThat(findFirst("ul.collection").find("li")).hasSize(2);
     }
 }
