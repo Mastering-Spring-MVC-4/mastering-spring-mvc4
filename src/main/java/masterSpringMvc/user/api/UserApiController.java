d156b8e25e68771f1522920890ffc02ee90f55cb
geowarin <geowarin@gmail.com>
2015-08-03 05:09:52 +0200

    Test PUT and DELETE on the user API controller

--- src/main/java/masterSpringMvc/user/api/UserApiController.java
+++ src/main/java/masterSpringMvc/user/api/UserApiController.java
@@ -39,7 +39,7 @@ public class UserApiController {
     @RequestMapping(value = "/user/{email}", method = RequestMethod.PUT)
     public ResponseEntity<User> updateUser(@PathVariable String email, @RequestBody User user) throws EntityNotFoundException {
         User saved = userRepository.update(email, user);
-        return new ResponseEntity<>(saved, HttpStatus.CREATED);
+        return new ResponseEntity<>(saved, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/user/{email}", method = RequestMethod.DELETE)
