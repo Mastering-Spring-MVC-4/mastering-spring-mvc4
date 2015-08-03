72b90691a2b434b30a9263996ab29c3609bcb97e
geowarin <geowarin@gmail.com>
2015-08-03 11:29:48 +0200

    Replaced HomeControllerTest with a groovy spec

--- src/test/groovy/masterSpringMvc/controller/HomeControllerSpec.groovy
+++ src/test/groovy/masterSpringMvc/controller/HomeControllerSpec.groovy
@@ -2,11 +2,14 @@ package masterSpringMvc.controller
 
 import masterSpringMvc.MasterSpringMvcApplication
 import masterSpringMvc.search.StubTwitterSearchConfig
+import masterSpringMvc.utils.SessionBuilder
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.SpringApplicationContextLoader
+import org.springframework.mock.web.MockHttpSession
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.web.WebAppConfiguration
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.ResultActions
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
 import spock.lang.Specification
@@ -35,5 +38,19 @@ class HomeControllerSpec extends Specification {
         response
                 .andExpect(status().isFound())
                 .andExpect(redirectedUrl("/profile"))
+    }
+
+    def "Should redirect to tastes when the profile is completed"() {
+        given: "My user is in session with tastes saved"
+        MockHttpSession session = new SessionBuilder().userTastes("spring", "groovy").build();
+
+        when: "When I go to the home page"
+        ResultActions response = this.mockMvc.perform(get("/")
+                .session(session))
+
+        then: "I'm taken to the result page"
+        response
+                .andExpect(status().isFound())
+                .andExpect(redirectedUrl("/search/mixed;keywords=spring,groovy"));
     }
 }
