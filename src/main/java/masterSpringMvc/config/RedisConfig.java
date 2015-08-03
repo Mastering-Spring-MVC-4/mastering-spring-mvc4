7e3750846e5d5e4439630edd6da54bcab02682b8
geowarin <geowarin@gmail.com>
2015-08-03 18:27:33 +0200

    Use Redis as a cache provider when the redis profile is active

--- src/main/java/masterSpringMvc/config/RedisConfig.java
+++ src/main/java/masterSpringMvc/config/RedisConfig.java
@@ -1,11 +1,35 @@
 package masterSpringMvc.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+import java.util.Arrays;
 
 @Configuration
 @Profile("redis")
 @EnableRedisHttpSession
 public class RedisConfig {
+
+    @Bean(name = "objectRedisTemplate")
+    public RedisTemplate objectRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
+
+    @Primary @Bean
+    public CacheManager cacheManager(@Qualifier("objectRedisTemplate") RedisTemplate template) {
+        RedisCacheManager cacheManager = new RedisCacheManager(template);
+        cacheManager.setCacheNames(Arrays.asList("searches"));
+        cacheManager.setDefaultExpiration(36_000);
+        return cacheManager;
+    }
 }
